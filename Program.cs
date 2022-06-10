62c52972ea4f6155af143845ceff4afc9f8e860f
Danya <theincredibledanzel@yandex.ru>
2022-06-10 19:26:25 +0400

    tr/fls altern

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿Console.Write("Vvedite chislo: ");
+﻿Console.Write("Koren: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Kvadrat: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-string input = Console.ReadLine(); // считывыет пользовательский ввод
-int number = Convert.ToInt32(input);
-
-int result = number * number;
-
-Console.Write("Otvet: ");
-Console.Write(result);
+if (number1 * number1 == number2)
+    Console.WriteLine(number1 + "^2" + "=" + number2 + " (True) ");
+else
+    Console.WriteLine(number1 + "^2" + "=" + number2 + " (False) ");
