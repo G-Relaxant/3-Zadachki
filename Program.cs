9d58408bf909845054e98f40827a0811c426cbbf
Danya <theincredibledanzel@yandex.ru>
2022-06-10 08:16:28 +0400

    prog read-write

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Moto! Hi-hi^^");
+﻿string input = Console.ReadLine(); // считывыет пользовательский ввод
+int number = Convert.ToInt32(input);
+Console.WriteLine(input);
+
+//программа будет выводить введенное мной число, но не слова
